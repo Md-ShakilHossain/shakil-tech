035e801176d3990a5bba69d73f7aca1ccc1143b6
Md-ShakilHossain <shakilh550@gmail.com>
2023-10-21 02:32:12 +0600

    requirement nine completed

--- src/Pages/BrandProducts/ProductDetails.jsx
+++ src/Pages/BrandProducts/ProductDetails.jsx
@@ -1,11 +1,43 @@
+import { useContext } from "react";
 import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../AuthProvider/AuthProvider";
+import Swal from 'sweetalert2'
 
 
 const ProductDetails = () => {
 
     const product = useLoaderData();
+    const user = useContext(AuthContext);
+
+    const userEmail = user.user.email;
 
     const { imageURL, name, brandName, shortDescription, rating, price } = product;
+
+    const cartData = { ...product, userEmail};
+
+    const handleAddToCart = () => {
+
+        fetch('http://localhost:5000/carts', {
+            method: 'POST',
+            headers: {
+                'content-type':'application/json'
+            },
+            body: JSON.stringify(cartData)
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.insertedId){
+                Swal.fire({
+                    title: 'Success!',
+                    text: 'Cart Added Successfully',
+                    icon: 'success',
+                    confirmButtonText: 'Okay'
+                  })
+            }
+            console.log(data);
+        })
+
+    }
 
     return (
         <div className="w-4/5 mx-auto my-20 bg-slate-200 p-6">
@@ -23,7 +55,8 @@ const ProductDetails = () => {
 
                     <p className="font-medium">{shortDescription}</p>
 
-                    <button className="btn w-full mt-10 shadow-xl">Add to Cart</button>
+                    <button onClick={handleAddToCart}
+                    className="btn w-full mt-10 shadow-xl">Add to Cart</button>
                 </div>
             </div>
         </div>
